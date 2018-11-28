6d9d977774b419da3fa4790e6460389360fcc4f2
Daniel Lopes <danielorkae@outlook.com>
2018-11-27 22:38:07 -0300

    added migrations

---
+++ database/migrations/2018_11_28_012436_create_phones_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('people_id')->unsigned();
+            $table->string('phone');
+            $table->boolean('primary');            
+            $table->timestamps();
+
+            $table->foreign('people_id')
+                ->references('id')
+                ->on('people');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
